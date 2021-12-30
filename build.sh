b87a928e3edb87c6693669b28397f932bf9c2c68
Jason Morley <hello@jbmorley.co.uk>
2021-12-30 01:28:34 +0000

    Move the Debian packages into a dedicated Raspbian directory

--- build.sh
+++ build.sh
@@ -6,9 +6,15 @@ set -u
 
 ROOT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ANSIBLE_DIRECTORY="${ROOT_DIRECTORY}/ansible"
+BUILD_DIRECTORY="${ROOT_DIRECTORY}/build"
+
+if [ -d "${BUILD_DIRECTORY}" ] ; then
+    rm -r "${BUILD_DIRECTORY}"
+fi
+mkdir -p "${BUILD_DIRECTORY}"
 
 "${ROOT_DIRECTORY}/package-repository.py" \
-    --output "${ROOT_DIRECTORY}/build" \
+    --output "${BUILD_DIRECTORY}/raspbian" \
     "inseven/elsewhere"
 
 pushd "${ANSIBLE_DIRECTORY}"
